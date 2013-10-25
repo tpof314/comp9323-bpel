6a4286a1eeb4cef0563d0be1aaf8f89b913076ae
Duancy <duancy@ubuntu.ubuntu-domain>
2013-10-25 22:36:06 +1100

    1 problem left

--- src/main/java/servlets/createAssignment.java
+++ src/main/java/servlets/createAssignment.java
@@ -1,16 +1,9 @@
 package servlets;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.Iterator;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,13 +12,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.disk.DiskFileItemFactory;
-import org.apache.commons.fileupload.servlet.ServletFileUpload;
-
 import model.Assignment;
 import model.bean.UserBean;
-import sun.misc.IOUtils;
 
 /**
  * Servlet implementation class createAssignment
@@ -34,7 +22,6 @@ import sun.misc.IOUtils;
 public class createAssignment extends HttpServlet {
 	
 	private static final long serialVersionUID = 1L;
-	private ServletFileUpload uploader = null;
 	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
@@ -45,7 +32,7 @@ public class createAssignment extends HttpServlet {
 		Date deadline = null;
 		boolean flag = false;
 		
-		int newAssNo = userBean.getAssignments().size() + 1;
+		
 		
 		String dateFormat = "dd/MM/yyyy HH:mm:ss";
 		SimpleDateFormat sdf = new SimpleDateFormat(dateFormat);
@@ -56,6 +43,8 @@ public class createAssignment extends HttpServlet {
 		assignment_name  = request.getParameter("new_assignment_name");
 		date_str  = request.getParameter("new_assignment_deadline");
 		spec = request.getParameter("new_assignment_specification");
+		
+		int newAssNo = userBean.getAssignments().size() + 1;
 		
 		
 		try {
